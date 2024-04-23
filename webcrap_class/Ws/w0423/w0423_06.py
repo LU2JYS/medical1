d7326ecbbfbe95864747ed31d187f44ea39f821b
전용성 <162073406+LU2JYS@users.noreply.github.com>
2024-04-23 23:02:31 +0900

    Update w0423_06.py

--- webcrap_class/Ws/w0423/w0423_06.py
+++ webcrap_class/Ws/w0423/w0423_06.py
@@ -1,40 +1,54 @@
-import time
-from selenium import webdriver
+# https://jumin.mois.go.kr/ageStatMonth.do
+# 서울특별시, 인천직할시, 경기도 3개의 인구를 웹스크래핑해서
+# 서울특별시 : 인구
+# 인천직할시 : 인구
+# 경기도 : 인구
+# 합계 : 인구를 출력하시오.
+import requests
 from bs4 import BeautifulSoup
-
-# Launch the Chrome browser
-browser = webdriver.Chrome()
-
-# URL to navigate
 url = "https://jumin.mois.go.kr/ageStatMonth.do"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36","Accept-Language":"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"}
+res = requests.get(url,headers=headers)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text,"lxml")
 
 
-browser.get(url)
+
+
+
 #서울,인천,경기도 인구를 웹스크래핑해서 
 #서울 : 총인구
 #인천 : 총인구
 #경기도 : 총인구
 #합계 : 인구
 
-time.sleep(3)
 
 
-soup = BeautifulSoup(browser.page_source, "lxml")
-
-
-list_ul = soup.find("div", {"class": "tab_section current_table title_table_div"})
-
-
-lis = list_ul.find_all("tr")
-print("-" * 40)
-for td in lis:
-    print("-" * 40)
-    print("서울 총인구수 : ", td.find("td", {"title": "2024년 03월 / 계"}))
+section = soup.findAll("td",{"class":"td_admin th1"})[1]
+for text in section:
+    print(text.get_text())
+total_p = soup.findAll("td",{"title":"2024년 03월 / 계"})[2]
+for text in total_p:
+    print(text.get_text())
     
-
-
-
-browser.quit()
+    section = soup.findAll("td",{"class":"td_admin th1"})[4]
+for text in section:
+    print(text.get_text())
+total_p = soup.findAll("td",{"title":"2024년 03월 / 계"})[3]
+for text in total_p:
+    print(text.get_text())
+    
+    section = soup.findAll("td",{"class":"td_admin th1"})[9]
+for text in section:
+    print(text.get_text())
+total_p = soup.findAll("td",{"title":"2024년 03월 / 계"})[4]
+for text in total_p:
+    print(text.get_text())
+    
+print("-"*40)
+for text in total_p:
+    print("합계 :", text.get_text())
 
 
 
